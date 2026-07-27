61dd83af0442eaa2ea1ef678172b9710561de7c8
vasu <vasumattipelli77@gmail.com>
2026-07-27 13:01:27 +0530

    shell

--- 03-installation-script.sh
+++ 03-installation-script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ] then
+if [ $USER_ID -ne 0 ]; then
     echo "run the script with root privilage"
 fi
 
 dnf install nginx -y
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ]; then
     echo " nginx installation failed "
 else 
     echo " nginx installation success "
