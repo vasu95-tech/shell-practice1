14561ca30405129ca9325af17f837e32edaafee8
vasu <vasumattipelli77@gmail.com>
2026-07-27 15:40:25 +0530

    shell

---
+++ o4-logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#LOG_FILE="$LOG_FOLDER/$(echo "$0 | cut -d "." -f1").log"
+LOG_FOLDER="/var/log/shell-scripting"
+mkdir -p $LOG_FOLDER
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+
+USER_ID=$(id -u)
+if [ USER_ID -ne 0 ]; then
+    echo -e "$R error $N run the script with root user"
+fi
+validate(){
+    if [ $1 -ne 0 ]; then
+     echo -e "installing $2 is $R failure $N" | tee -a $LOG_FILE
+    else
+     echo -e "installing $2 is $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    validate $? nginx
+else 
+    echo -e "nginx is already exist $Y skipping $N" | tee -a $LOG_FILE
+fi
+
+    
